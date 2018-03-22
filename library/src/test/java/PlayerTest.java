a2f1224c9727ee5fa1a6ee54d2b9c34825ec7b88
ashish mahindrakar <ashishmahi46@gmail.com>
2018-03-22 13:09:11 +0530

    added method to get all moves of player

--- library/src/test/java/PlayerTest.java
+++ library/src/test/java/PlayerTest.java
@@ -2,6 +2,8 @@ import com.tictactoe.Player;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
@@ -24,5 +26,13 @@ public class PlayerTest {
     public void should_add_move_to_players_moves() {
         player.addMove(1);
         assertThat(player.getMoves(),hasItem(1));
+    }
+
+    @Test
+    public void should_give_moves_of_player() {
+        ArrayList<Integer> expectedMoves = new ArrayList<Integer>();
+        expectedMoves.add(5);
+        player.addMove(5);
+        assertThat(player.getMoves(),is(expectedMoves));
     }
 }
