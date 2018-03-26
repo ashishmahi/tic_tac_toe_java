5c6f6bf0b1d885b135687e32bfdd364c126cea1b
ashish mahindrakar <ashishmahi46@gmail.com>
2018-03-26 10:11:14 +0530

    prevented player from re-selecting a cell.

--- application/src/main/java/com/tictactoe/TTTUsingAwt.java
+++ application/src/main/java/com/tictactoe/TTTUsingAwt.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,41 +10,37 @@ public class TTTUsingAwt {
     public static void main(String[] args) {
         game.addPlayer("ashish","X");
         game.addPlayer("debu","O");
-        TTTUsingAwt ttt = new TTTUsingAwt();
-        Frame f=new Frame("TicTacToe");
-        createBoard(f);
-    }
-
-//    private static void addEventListnerTo(Frame f) {
-//        Component[] components = f.getComponents();
-//        for (int i = 0; i < components.length; i++) {
-//
-//        }
-//    }
-
-    private static void createBoard(Frame f) {
+        JFrame f=new JFrame("TicTacToe");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(500,500);
+        createBoard(f);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    private static void createBoard(Frame f) {
+
         int yPos = 50;
+        int id = 0;
+        ActionListener[] l =new ActionListener[9];
         for (int i = 0; i < 3; i++) {
             int xPos = 50;
             for (int j = 0; j < 3; j++) {
-                Button b=new Button();
+                JButton b=new JButton();
                 b.setBounds(xPos,yPos,50,50);
                 f.add(b);
                 xPos+=50;
-                b.addActionListener(e -> {
-                    b.setLabel(game.getSymbol());
-                    int move = Integer.parseInt(b.getName().substring(6))+1;
-                    System.out.println(move);
-                    System.out.println(b.getName());
-                    game.updateMove(Integer.parseInt(b.getName().substring(6))+1);
-                });
+                final int cId = id;
+                l[cId] = (ActionEvent e) -> {
+                    b.setText(game.getSymbol());
+                    game.updateMove(cId+1);
+                    b.removeActionListener(l[cId]);
+                };
+                b.addActionListener(l[cId]);
+                id++;
             }
             yPos+=50;
         }
-
     }
 
 }
